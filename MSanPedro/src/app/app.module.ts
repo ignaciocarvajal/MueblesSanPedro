9b8d46dd8ddcae710c4c6ab96e77223032e5afcb
Ignacio Carvajal Duran <ignacio.carvajal@inacapmail.cl>
2018-03-23 02:59:06 -0300

    added ng-bootstrap in app-module
    se agrego el modulo de ng-bootstrap

--- MSanPedro/src/app/app.module.ts
+++ MSanPedro/src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
+//view
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
